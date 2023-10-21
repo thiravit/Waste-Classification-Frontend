647cd4d4cdfbdee8388c33bd98ef25378de8cc99
Wasu <riversea43@gmail.com>
2023-10-21 20:18:27 +0700

    clean some element

---
+++ app/home/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <main className="bg-background h-screen">
+            <div className="p-32">
+                <h1 className="text-white">slide</h1>
+
+            </div>
+        </main>
+    );
+
+}
